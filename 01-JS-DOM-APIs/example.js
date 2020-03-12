3cb62aa4acfa0c2b32176745d46788a6bf34b9ba
Antonella <antonellabuono4@gmail.com>
2020-03-12 02:09:50 -0300

    Ejercicio 3, agregue normalize.css

--- 01-JS-DOM-APIs/example.js
+++ 01-JS-DOM-APIs/example.js
@@ -5,3 +5,25 @@ function callbutton()
 {
     alert("Se ha llamado al boton");
 }
+
+function clickbutton() {
+        fetchDatos();
+}
+
+document.getElementById('boton').onclick = clickbutton; 
+    
+
+function fetchDatos()
+{
+    fetch('http://api.icndb.com/jokes/random')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        hidden.innerHTML = json.value.joke;
+    })
+    .catch(function(err){
+        console.error(err);
+        section.style.background = 'red';
+    });
+}
